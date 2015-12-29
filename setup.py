dec3e37c05ad6ad8cee16846887dd829d6452898
mFoxRU <mfoxru@gmail.com>
2015-12-29 10:56:28 +0300

    Added: setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='pyqt-websocket-client',
+        version='1.0',
+        packages=[''],
+        url='',
+        license='MIT',
+        author='mFoxRU',
+        author_email='',
+        description='',
+        install_requires=['twisted', 'autobahn', 'qt4reactor', 'PyQt4']
+)
